689cb38aa151092f4cd43f7aba116412dc949b82
RomanMeln <tupozarabotok@gmail.com>
2022-02-07 21:26:51 +0300

    Тестирование

--- authapp/tests.py
+++ authapp/tests.py
@@ -1,3 +1,48 @@
 from django.test import TestCase
 
-# Create your tests here.
+from authapp.models import ShopUser
+
+
+
+class TestUserManagement(TestCase):
+    username = 'django'
+    password = 'geekbrains'
+    email = 'django@gb.local'
+
+
+
+    def setUp(self) -> None:
+
+        self.user = ShopUser.objects.create_superuser(
+            username=self.username,
+            password=self.password,
+            email=self.email,
+        )
+
+    def test_user_login(self):
+        # главная без логина
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+        self.assertTrue(response.context['user'].is_anonymous)
+
+        # данные пользователя
+        self.client.login(username=self.username, password=self.password)
+
+        # логинимся
+        response = self.client.get('/auth/login/')
+        self.assertFalse(response.context['user'].is_anonymous)
+
+
+    def test_user_logout(self):
+        # главная после логина
+        self.client.login(username=self.username, password=self.password)
+        response = self.client.get('/auth/login/')
+
+        self.assertFalse(response.context['user'].is_anonymous)
+
+        self.client.get('/auth/logout/')
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.get('/')
+        self.assertTrue(response.context['user'].is_anonymous)
--- mainapp/tests.py
+++ mainapp/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from mainapp.models import Product, ProductCategory
 
-# Create your tests here.
+
+class TestMainappSmoke(TestCase):
+    def setUp(self) -> None:
+        category = ProductCategory.objects.create(
+            name='cat1'
+        )
+        for i in range(10):
+            Product.objects.create(
+                category=category,
+                name=f'prod#{i}',
+                description='test'
+            )
+
+    def test_mainapp_urls(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_categories_urls(self):
+        for category in ProductCategory.objects.all():
+            response = self.client.get(f'/products/{category.pk}/')
+            self.assertEqual(response.status_code, 200)
+
+    def test_products_urls(self):
+        for product in Product.objects.all():
+            response = self.client.get(f'/products/product/{product.pk}/')
+            self.assertEqual(response.status_code, 200)
